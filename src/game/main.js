49494c78f8272cc6eb134bcc4eaa116b0708f6da
Sean Bohan <pixelpicosean@gmail.com>
2017-07-22 09:43:15 +0800

    Cleanup test code

--- src/game/main.js
+++ src/game/main.js
@@ -3,7 +3,39 @@ import settings from './settings';
 
 
 class CustomArea extends v.Area2D {
+    constructor() {
+        super();
+
+        this._debug_shape = null;
+    }
     _enter_tree() {
+        this.set_shape(new v.RectangleShape2D(16, 16));
+        this.set_collision_layer_bit(1, true);
+        this.set_collision_mask_bit(2, true);
+
+        this._debug_shape = new v.Graphics();
+        this._debug_shape.clear();
+        this._debug_shape.begin_fill(0xffffff);
+        this._debug_shape.draw_rect(-this._shape.extents.x, -this._shape.extents.y, this._shape.extents.x * 2, this._shape.extents.y * 2);
+        this._debug_shape.end_fill();
+        this.add_child(this._debug_shape);
+
+        this.body_entered.add((who) => console.log(`${who.name} enter`));
+        this.body_exited.add((who) => console.log(`${who.name} exit`));
+    }
+}
+
+
+class StaticBody extends v.PhysicsBody2D {
+    constructor() {
+        super();
+
+        this._debug_shape = null;
+    }
+    _enter_tree() {
+        this.set_shape(new v.RectangleShape2D(40, 20));
+        this.set_collision_layer_bit(2, true);
+
         this._debug_shape = new v.Graphics();
         this._debug_shape.clear();
         this._debug_shape.begin_fill(0xffffff);
@@ -11,17 +43,8 @@ class CustomArea extends v.Area2D {
         this._debug_shape.end_fill();
         this.add_child(this._debug_shape);
     }
-}
-
-
-class StaticBody extends v.PhysicsBody2D {
-    _enter_tree() {
-        this._debug_shape = new v.Graphics();
-        this._debug_shape.clear();
-        this._debug_shape.begin_fill(0xffffff);
-        this._debug_shape.draw_rect(-this._shape.extents.x, -this._shape.extents.y, this._shape.extents.x * 2, this._shape.extents.y * 2);
-        this._debug_shape.end_fill();
-        this.add_child(this._debug_shape);
+    collide(body, res) {
+        return false;
     }
 }
 
@@ -30,6 +53,7 @@ class CustomBody extends v.PhysicsBody2D {
     constructor() {
         super();
 
+        this._debug_shape = null;
         this.velocity = new v.Vector(0, 0);
     }
     _enter_tree() {
@@ -79,20 +103,12 @@ class Scene extends v.Node2D {
         const a = new CustomArea();
         a.name = 'a';
         a.position.set(100, 200);
-        a.set_shape(new v.RectangleShape2D(16, 16));
-        a.set_collision_layer_bit(1, true);
-        a.set_collision_mask_bit(2, true);
-        a.body_entered.add((who) => console.log(`${who.name} enter`));
-        a.body_exited.add((who) => console.log(`${who.name} exit`));
         this.add_child(a);
         this.a = a;
 
         const b = new StaticBody();
         b.name = 'b';
         b.position.set(300, 200);
-        b.set_shape(new v.RectangleShape2D(40, 20));
-        b.set_collision_layer_bit(2, true);
-        b.collide = () => false;
         this.add_child(b);
         this.b = b;
 
