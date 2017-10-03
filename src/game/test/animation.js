e3ee4ccff8340215513b31cb57ff76c95577f6af
Sean Bohan <pixelpicosean@gmail.com>
2017-10-04 06:36:15 +0800

    Update animation test due to API changes

--- src/game/test/animation.js
+++ src/game/test/animation.js
@@ -11,7 +11,7 @@ export default class AnimationTest extends v.Node2D {
         let icon = this.add_child(new v.Sprite('icon'));
         icon.position.set(50, 100);
 
-        let tween = icon.tweens.create();
+        let tween = new v.Tween();
         tween.interpolate_property(icon, 'position.x', 50, 150, 1, 'Quadratic.InOut', 2);
         tween.interpolate_property(icon, 'position.x', 150, 50, 1, 'Quadratic.InOut', 3);
         tween.interpolate_property(icon, 'tint', 0x000000, 0xFFFFFF, 2, 'Quadratic.InOut', 2);
@@ -22,6 +22,8 @@ export default class AnimationTest extends v.Node2D {
         tween.interpolate_deferred_callback(this, 3, 'deferred_greet', 'Sean');
         tween.interpolate_callback(this, 3, 'greet', 'Sean');
         tween.start();
+
+        icon.tweens.add(tween);
     }
     _process(delta) {}
     _exit_tree() {}
@@ -34,6 +36,6 @@ export default class AnimationTest extends v.Node2D {
     }
 
     fly(num) {
-        console.log(`fly: ${num | 0}`);
+        // console.log(`fly: ${num | 0}`);
     }
 }
