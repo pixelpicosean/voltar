afb3e8ac04365dcdbd2277ad8cfa5deb14648342
Sean Bohan <pixelpicosean@gmail.com>
2017-10-05 07:44:44 +0800

    BackgroundMap upgrade to latest

--- src/engine/core/scene/map/BackgroundMap.js
+++ src/engine/core/scene/map/BackgroundMap.js
@@ -1,3 +1,4 @@
+// Based on pixi-tilemap master~02218a043cacd310b49e2801a117423cb46b23d3
 import Node2D from '../Node2D';
 
 import { TextureCache } from '../../utils';
@@ -16,10 +17,6 @@ export default class BackgroundMap extends Node2D {
 
         this.tile_width = tile_width;
         this.tile_height = tile_height;
-
-        // Fix/remove square rendering mode
-        // this.use_square = (tile_width === tile_height);
-        this.use_square = false;
 
         this.pointsBuf = [];
         this._tempSize = new Float32Array([0, 0]);
@@ -70,7 +67,36 @@ export default class BackgroundMap extends Node2D {
     _push_tile(u, v, x, y, animX, animY) {
         var pb = this.pointsBuf;
         this.hasAnim = this.hasAnim || animX > 0 || animY > 0;
-        if (this.use_square) {
+
+        var i;
+        if (this.tile_width % this.tile_height === 0) {
+            //horizontal line on squares
+            for (i = 0; i < this.tile_width / this.tile_height; i++) {
+                pb.push(u + i * this.tile_height);
+                pb.push(v);
+                pb.push(x + i * this.tile_height);
+                pb.push(y);
+                pb.push(this.tile_height);
+                pb.push(this.tile_height);
+                pb.push(animX | 0);
+                pb.push(animY | 0);
+                pb.push(0);
+            }
+        } else if (this.tile_height % this.tile_width === 0) {
+            //vertical line on squares
+            for (i = 0; i < this.tile_height / this.tile_width; i++) {
+                pb.push(u);
+                pb.push(v + i * this.tile_width);
+                pb.push(x);
+                pb.push(y + i * this.tile_width);
+                pb.push(this.tile_width);
+                pb.push(this.tile_width);
+                pb.push(animX | 0);
+                pb.push(animY | 0);
+                pb.push(0);
+            }
+        } else {
+            //ok, ok, lets use rectangle. but its not working with square shader yet
             pb.push(u);
             pb.push(v);
             pb.push(x);
@@ -80,46 +106,6 @@ export default class BackgroundMap extends Node2D {
             pb.push(animX | 0);
             pb.push(animY | 0);
             pb.push(0);
-        } else {
-            var i;
-            if (this.tile_width % this.tile_height === 0) {
-                //horizontal line on squares
-                for (i = 0; i < this.tile_width / this.tile_height; i++) {
-                    pb.push(u + i * this.tile_height);
-                    pb.push(v);
-                    pb.push(x + i * this.tile_height);
-                    pb.push(y);
-                    pb.push(this.tile_height);
-                    pb.push(this.tile_height);
-                    pb.push(animX | 0);
-                    pb.push(animY | 0);
-                    pb.push(0);
-                }
-            } else if (this.tile_height % this.tile_width === 0) {
-                //vertical line on squares
-                for (i = 0; i < this.tile_height / this.tile_width; i++) {
-                    pb.push(u);
-                    pb.push(v + i * this.tile_width);
-                    pb.push(x);
-                    pb.push(y + i * this.tile_width);
-                    pb.push(this.tile_width);
-                    pb.push(this.tile_width);
-                    pb.push(animX | 0);
-                    pb.push(animY | 0);
-                    pb.push(0);
-                }
-            } else {
-                //ok, ok, lets use rectangle. but its not working with square shader yet
-                pb.push(u);
-                pb.push(v);
-                pb.push(x);
-                pb.push(y);
-                pb.push(this.tile_width);
-                pb.push(this.tile_height);
-                pb.push(animX | 0);
-                pb.push(animY | 0);
-                pb.push(0);
-            }
         }
     }
 
@@ -200,18 +186,11 @@ export default class BackgroundMap extends Node2D {
 
         // Start to render
         var gl = renderer.gl;
-        var shader = renderer.plugins.tilemap.getShader(this.use_square);
+        var shader = renderer.plugins.tilemap.getShader();
         renderer.setObjectRenderer(renderer.plugins.tilemap);
         renderer.bindShader(shader);
         renderer._activeRenderTarget.projectionMatrix.copy(this._globalMat).append(this.world_transform);
         shader.uniforms.projectionMatrix = this._globalMat.to_array(true);
-        if (this.use_square) {
-            var tempScale = this._tempScale;
-            tempScale[0] = this._globalMat.a >= 0 ? 1 : -1;
-            tempScale[1] = this._globalMat.d < 0 ? 1 : -1;
-            var ps = shader.uniforms.pointScale = tempScale;
-            shader.uniforms.projectionScale = Math.abs(this.world_transform.a) * renderer.resolution;
-        }
         // var af = shader.uniforms.animationFrame = renderer.plugins.tilemap.tileAnim;
         //shader.syncUniform(shader.uniforms.animationFrame);
 
@@ -220,9 +199,7 @@ export default class BackgroundMap extends Node2D {
         var rectsCount = points.length / 9;
         var tile = renderer.plugins.tilemap;
         var gl = renderer.gl;
-        if (!this.use_square) {
-            tile.checkIndexBuffer(rectsCount);
-        }
+        tile.checkIndexBuffer(rectsCount);
 
         var textures = this.textures;
         if (textures.length === 0) return;
@@ -243,7 +220,7 @@ export default class BackgroundMap extends Node2D {
         //lost context! recover!
         var vb = tile.getVb(this.vbId);
         if (!vb) {
-            vb = tile.createVb(this.use_square);
+            vb = tile.createVb();
             this.vbId = vb.id;
             this.vbBuffer = null;
             this.modificationMarker = 0;
@@ -275,77 +252,62 @@ export default class BackgroundMap extends Node2D {
             var sz = 0;
             //var tint = 0xffffffff;
             var textureId, shiftU, shiftV;
-            if (this.use_square) {
-                for (i = 0; i < points.length; i += 9) {
-                    textureId = (points[i + 8] >> 2);
-                    shiftU = 1024 * (points[i + 8] & 1);
-                    shiftV = 1024 * ((points[i + 8] >> 1) & 1);
-                    arr[sz++] = points[i + 2];
-                    arr[sz++] = points[i + 3];
-                    arr[sz++] = points[i + 0] + shiftU;
-                    arr[sz++] = points[i + 1] + shiftV;
-                    arr[sz++] = points[i + 4];
-                    arr[sz++] = points[i + 6];
-                    arr[sz++] = points[i + 7];
-                    arr[sz++] = textureId;
-                }
-            } else {
-                //var tint = 0xffffffff;
-                var tint = -1;
-                for (i = 0; i < points.length; i += 9) {
-                    var eps = 0.5;
-                    textureId = (points[i + 8] >> 2);
-                    shiftU = 1024 * (points[i + 8] & 1);
-                    shiftV = 1024 * ((points[i + 8] >> 1) & 1);
-                    var x = points[i + 2], y = points[i + 3];
-                    var w = points[i + 4], h = points[i + 5];
-                    var u = points[i] + shiftU, v = points[i + 1] + shiftV;
-                    var animX = points[i + 6], animY = points[i + 7];
-                    arr[sz++] = x;
-                    arr[sz++] = y;
-                    arr[sz++] = u;
-                    arr[sz++] = v;
-                    arr[sz++] = u + eps;
-                    arr[sz++] = v + eps;
-                    arr[sz++] = u + w - eps;
-                    arr[sz++] = v + h - eps;
-                    arr[sz++] = animX;
-                    arr[sz++] = animY;
-                    arr[sz++] = textureId;
-                    arr[sz++] = x + w;
-                    arr[sz++] = y;
-                    arr[sz++] = u + w;
-                    arr[sz++] = v;
-                    arr[sz++] = u + eps;
-                    arr[sz++] = v + eps;
-                    arr[sz++] = u + w - eps;
-                    arr[sz++] = v + h - eps;
-                    arr[sz++] = animX;
-                    arr[sz++] = animY;
-                    arr[sz++] = textureId;
-                    arr[sz++] = x + w;
-                    arr[sz++] = y + h;
-                    arr[sz++] = u + w;
-                    arr[sz++] = v + h;
-                    arr[sz++] = u + eps;
-                    arr[sz++] = v + eps;
-                    arr[sz++] = u + w - eps;
-                    arr[sz++] = v + h - eps;
-                    arr[sz++] = animX;
-                    arr[sz++] = animY;
-                    arr[sz++] = textureId;
-                    arr[sz++] = x;
-                    arr[sz++] = y + h;
-                    arr[sz++] = u;
-                    arr[sz++] = v + h;
-                    arr[sz++] = u + eps;
-                    arr[sz++] = v + eps;
-                    arr[sz++] = u + w - eps;
-                    arr[sz++] = v + h - eps;
-                    arr[sz++] = animX;
-                    arr[sz++] = animY;
-                    arr[sz++] = textureId;
-                }
+
+            //var tint = 0xffffffff;
+            var tint = -1;
+            for (i = 0; i < points.length; i += 9) {
+                var eps = 0.5;
+                textureId = (points[i + 8] >> 2);
+                shiftU = 1024 * (points[i + 8] & 1);
+                shiftV = 1024 * ((points[i + 8] >> 1) & 1);
+                var x = points[i + 2], y = points[i + 3];
+                var w = points[i + 4], h = points[i + 5];
+                var u = points[i] + shiftU, v = points[i + 1] + shiftV;
+                var animX = points[i + 6], animY = points[i + 7];
+                arr[sz++] = x;
+                arr[sz++] = y;
+                arr[sz++] = u;
+                arr[sz++] = v;
+                arr[sz++] = u + eps;
+                arr[sz++] = v + eps;
+                arr[sz++] = u + w - eps;
+                arr[sz++] = v + h - eps;
+                arr[sz++] = animX;
+                arr[sz++] = animY;
+                arr[sz++] = textureId;
+                arr[sz++] = x + w;
+                arr[sz++] = y;
+                arr[sz++] = u + w;
+                arr[sz++] = v;
+                arr[sz++] = u + eps;
+                arr[sz++] = v + eps;
+                arr[sz++] = u + w - eps;
+                arr[sz++] = v + h - eps;
+                arr[sz++] = animX;
+                arr[sz++] = animY;
+                arr[sz++] = textureId;
+                arr[sz++] = x + w;
+                arr[sz++] = y + h;
+                arr[sz++] = u + w;
+                arr[sz++] = v + h;
+                arr[sz++] = u + eps;
+                arr[sz++] = v + eps;
+                arr[sz++] = u + w - eps;
+                arr[sz++] = v + h - eps;
+                arr[sz++] = animX;
+                arr[sz++] = animY;
+                arr[sz++] = textureId;
+                arr[sz++] = x;
+                arr[sz++] = y + h;
+                arr[sz++] = u;
+                arr[sz++] = v + h;
+                arr[sz++] = u + eps;
+                arr[sz++] = v + eps;
+                arr[sz++] = u + w - eps;
+                arr[sz++] = v + h - eps;
+                arr[sz++] = animX;
+                arr[sz++] = animY;
+                arr[sz++] = textureId;
             }
             // if (vs > this.vbArray.length/2 ) {
             vertexBuf.upload(arr, 0, true);
@@ -354,9 +316,6 @@ export default class BackgroundMap extends Node2D {
             //     vb.upload(view, 0);
             // }
         }
-        if (this.use_square)
-            gl.drawArrays(gl.POINTS, 0, vertices);
-        else
-            gl.drawElements(gl.TRIANGLES, rectsCount * 6, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, rectsCount * 6, gl.UNSIGNED_SHORT, 0);
     }
 }
