fcf768293f02168fd7d897d13164d8cbf5975b38
Sean Bohan <pixelpicosean@gmail.com>
2017-11-07 19:53:16 +0800

    Create a hard link to commander.scon and test transition

--- src/game/test/coa.js
+++ src/game/test/coa.js
@@ -3,7 +3,7 @@ import { Model } from 'engine/core/scene/coa_sprite/Model';
 import CoaSprite from 'engine/core/scene/coa_sprite/CoaSprite';
 
 v.loader.add('media/commander.json');
-v.loader.add('cc', 'media/commander/commander.scon');
+v.loader.add('cc', 'media/commander.scon');
 
 
 export default class CoaTest extends v.Node2D {
@@ -14,12 +14,45 @@ export default class CoaTest extends v.Node2D {
     _enter_tree() {
         const num = 32, col = 8;
         const scale = 0.25;
-        for (let i = 0; i < num; i++) {
-            let hero = this.add_child(new CoaSprite().load('cc', 0));
-            hero.scale.set(scale, -scale);
-            hero.position.set(20 + 30 * Math.floor(i % col), 40 + 40 * Math.floor(i / col));
-            hero.play('idle');
-        }
+        // for (let i = 0; i < num; i++) {
+        //     let hero = this.add_child(new CoaSprite().load('cc', 0));
+        //     hero.scale.set(scale, -scale);
+        //     hero.position.set(20 + 30 * Math.floor(i % col), 40 + 40 * Math.floor(i / col));
+        //     hero.play('idle');
+        // }
+
+        /**
+         * @type {CoaSprite}
+         */
+        let hero = this.add_child(new CoaSprite().load('cc', 0));
+        hero.position.set(128, 200);
+        hero.play('idle');
+        hero.animator.speed = 0.25;
+        setInterval(() => {
+            switch (hero.animator.current_animation.name) {
+                case 'idle':
+                    hero.animator.transition('walk', 0.5);
+                    break;
+                case 'walk':
+                    hero.animator.transition('jump', 0.5);
+                    break;
+                case 'jump':
+                    hero.animator.transition('doublejump', 0.5);
+                    break;
+                case 'doublejump':
+                    hero.animator.transition('fall', 0.5);
+                    break;
+                case 'fall':
+                    hero.animator.transition('wallslice', 0.5);
+                    break;
+                case 'wallslice':
+                    hero.animator.transition('atk', 0.5);
+                    break;
+                case 'atk':
+                    hero.animator.transition('idle', 0.5);
+                    break;
+            }
+        }, 4000);
 
         this.info = this.add_child(new v.BitmapText('0', {
             font: '04b03',
