bde39b1cfa8c4fce4f15bd03b593c945ae3bd215
Sean Bohan <pixelpicosean@gmail.com>
2018-10-16 16:36:21 +0800

    New extract sample

--- src/game/main.js
+++ src/game/main.js
@@ -4,17 +4,18 @@ import * as v from 'engine/index';
 // Import features you want to use
 /* import 'engine/accessibility/enable'; */
 /* import 'engine/interaction/enable'; */
+/* import 'engine/extract/enable'; */
 /* import 'engine/scene/coa/enable'; */
 /* import 'engine/scene/graphics/enable'; */
 /* import 'engine/scene/map/enable'; */
-import 'engine/scene/mesh/enable';
+/* import 'engine/scene/mesh/enable'; */
 import 'engine/scene/sprites/enable';
 /* import 'engine/scene/text/enable'; */
 /* import 'engine/tween/enable'; */
 
 // Scenes
 import Preloader from 'game/preloader/Preloader';
-import Test from 'game/test/Mesh';
+import Test from 'game/test/AnimatedSprite';
 
 v.preload('media/04b03.fnt');
 
