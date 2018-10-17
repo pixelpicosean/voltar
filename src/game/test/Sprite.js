a075321d2a69ce408cb8cc340290a6d3c89f4f32
Sean Bohan <pixelpicosean@gmail.com>
2018-10-17 17:57:34 +0800

    Add deleted samples back with camclecase name

---
+++ src/game/test/Sprite.js
@@ -0,0 +1,26 @@
+import * as v from 'engine/index';
+import * as filters from 'engine/filters/index';
+
+v.preload('icon', 'media/icon.png');
+
+export default class SpriteTest extends v.Node2D {
+    static instance() {
+        return new SpriteTest();
+    }
+
+    _enter_tree() {
+        let spr = this.add_child(new v.Sprite('icon'));
+        spr.position.set(100, 100);
+        spr.anchor.set(0.5, 0.5);
+        spr.filters = [
+            new filters.Noise(1),
+            new filters.Blur(1, 1, 1),
+        ];
+
+        let mask = this.add_child(new v.Graphics())
+            .begin_fill(0xFFFFFF)
+            .draw_circle(100, 100, 30)
+            .end_fill()
+        spr.mask = mask;
+    }
+}
