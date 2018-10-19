606f14342895ac6c50d6ef7fb80c8d9668db1b87
Sean Bohan <pixelpicosean@gmail.com>
2018-10-20 06:51:24 +0800

    Import settings converted from godot project

--- src/game/main.js
+++ src/game/main.js
@@ -18,31 +18,15 @@ import 'engine/scene/sprites/enable';
 import Preloader from 'game/preloader/Preloader';
 import Test from 'game/test/Sprite';
 
+import Settings from 'project.json';
+
 v.preload('media/04b03.fnt');
 
-v.scene_tree.init({
-    application: {
-        name: 'Voltar',
-        preloader: Preloader,
-        main_scene: Test,
-    },
-    display: {
-        view: 'game',
-        container: 'container',
-
-        width: 256,
-        height: 256,
-        resolution: 1,
-
-        background_color: 0x00AAC9,
-
-        antialias: false,
-        pixel_snap: true,
-        scale_mode: 'nearest',
-
-        FPS: 60,
-
-        stretch_mode: 'viewport',
-        stretch_aspect: 'keep',
-    },
-});
+v.scene_tree.init(
+    v.utils.deep_merge(Settings, {
+        application: {
+            preloader: Preloader,
+            main_scene: Test,
+        },
+    })
+);
