26fc7300a1a3b69255774e5df7a30e7b2bb5d6dc
Sean Bohan <pixelpicosean@gmail.com>
2018-10-27 16:09:51 +0800

    Cleanup SAT sample

--- src/game/test/SAT.js
+++ src/game/test/SAT.js
@@ -40,55 +40,26 @@ export default class SAT extends v.Node2D {
             .set_collision_layer_bit(1, true)
             .set_position(200, 300)
             .set_rotation(Math.PI * 0.1)
-        this.rect3 = this.add_child(new v.Graphics())
+        this.static_2.add_child(new v.Graphics())
+            .begin_fill(0xFFFF00, 0.6)
+            .draw_rect(-40, -10, 80, 20)
+            .end_fill()
 
         this.hero = this.add_child(new Box())
         this.hero.position.set(180, 200)
 
         this.body = this.add_child(new v.RigidBody2D())
-        this.body.add_shape(new v.RectangleShape2D(10, 10))
-        this.body.set_collision_layer_bit(2, true)
-        this.body.set_collision_mask_bit(1, true)
-        this.body.bounce = 0.3
-        this.body.position.set(230, 200);
-        this.rect = this.add_child(new v.Graphics())
-        this.body.add_child(new v.Graphics())
-            .begin_fill(0xFFFF00, 0.6)
-            .draw_rect(-10, -10, 20, 20)
-            .end_fill()
+            .add_shape(new v.RectangleShape2D(10, 10))
+            .set_collision_layer_bit(2, true)
+            .set_collision_mask_bit(1, true)
+            .set_bounce(0.3)
+            .set_position(230, 200)
+            .add_child(new v.Graphics())
+                .begin_fill(0xFFFF00, 0.6)
+                .draw_rect(-10, -10, 20, 20)
+                .end_fill()
 
         this.set_process(true)
     }
-    _process(delta) {
-        this.rect3.set_position(this.static_2.position)
-            .clear()
-
-            .set_line_style(1, 0xFFFFFF, 1, 0)
-            .begin_fill(0xFFFF00, 0.6)
-            .move_to(this.static_2.shapes[0].vertices[0].x, this.static_2.shapes[0].vertices[0].y)
-            .line_to(this.static_2.shapes[0].vertices[1].x, this.static_2.shapes[0].vertices[1].y)
-            .line_to(this.static_2.shapes[0].vertices[2].x, this.static_2.shapes[0].vertices[2].y)
-            .line_to(this.static_2.shapes[0].vertices[3].x, this.static_2.shapes[0].vertices[3].y)
-            .end_fill()
-
-            .set_line_style(1, 0xFFFFFF, 1, 0)
-            .move_to(0, 0)
-            .line_to(this.static_2.shapes[0].normals[0].x * 20, this.static_2.shapes[0].normals[0].y * 20)
-            .move_to(0, 0)
-            .line_to(this.static_2.shapes[0].normals[1].x * 20, this.static_2.shapes[0].normals[1].y * 20)
-
-        this.rect.set_position(this.body.position)
-            .clear()
-
-            .begin_fill(0x00FFFF, 0.6)
-            .move_to(this.body.shapes[0].vertices[0].x, this.body.shapes[0].vertices[0].y)
-            .line_to(this.body.shapes[0].vertices[1].x, this.body.shapes[0].vertices[1].y)
-            .line_to(this.body.shapes[0].vertices[2].x, this.body.shapes[0].vertices[2].y)
-            .line_to(this.body.shapes[0].vertices[3].x, this.body.shapes[0].vertices[3].y)
-            .end_fill()
-
-            .set_line_style(1, 0xFFFFFF, 1, 0)
-            .move_to(0, 0)
-            .line_to(this.body.linear_velocity.x, this.body.linear_velocity.y)
-    }
+    _process(delta) {}
 }
