0bf44dd22601fab1f5c523c9f2b7dfcabba0b767
Sean Bohan <pixelpicosean@gmail.com>
2018-10-28 22:45:42 +0800

    Add more fields to StaticBody2D and load data

--- src/engine/scene/physics/StaticBody2D.js
+++ src/engine/scene/physics/StaticBody2D.js
@@ -1,12 +1,35 @@
 import { CollisionObjectTypes } from './CollisionObject2D';
 import PhysicsBody2D from './PhysicsBody2D';
 import { node_class_map } from 'engine/registry';
+import { Vector2 } from 'engine/math/index';
 
 export default class StaticBody2D extends PhysicsBody2D {
+    get linear_velocity() {
+        return this.constant_linear_velocity;
+    }
     constructor() {
         super();
 
         this.collision_object_type = CollisionObjectTypes.STATIC;
+
+        this.constant_linear_velocity = new Vector2();
+        this.constant_angular_velocity = 0;
+    }
+    _load_data(data) {
+        super._load_data(data);
+
+        for (let k in data) {
+            switch (k) {
+                case 'constant_linear_velocity': {
+                    this.constant_linear_velocity.copy(data[k]);
+                } break;
+                case 'constant_angular_velocity': {
+                    this[k] = data[k];
+                } break;
+            }
+        }
+
+        return this;
     }
 }
 
