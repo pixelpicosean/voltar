a4745d6c03dc4e8485615448120a6be948cdbb94
Sean Bohan <pixelpicosean@gmail.com>
2018-10-28 22:45:18 +0800

    Add more fields to Area2D and load data

--- src/engine/scene/physics/Area2D.js
+++ src/engine/scene/physics/Area2D.js
@@ -1,6 +1,7 @@
 import CollisionObject2D, { CollisionObjectTypes } from './CollisionObject2D';
 import { Signal } from 'engine/dep/index';
 import { node_class_map } from 'engine/registry';
+import { Vector2 } from 'engine/math/index';
 
 export default class Area2D extends CollisionObject2D {
     constructor() {
@@ -9,6 +10,13 @@ export default class Area2D extends CollisionObject2D {
         this.type = 'Area2D';
 
         this.collision_object_type = CollisionObjectTypes.AREA;
+
+        this.gravity_point = false;
+        this.gravity_distance_scale = 0;
+        this.gravity_vec = new Vector2();
+        this.gravity = 0;
+        this.linear_damp = 0.1;
+        this.angular_damp = 1;
 
         this.touched_areas = [];
         this.prev_touched_areas = [];
@@ -26,7 +34,16 @@ export default class Area2D extends CollisionObject2D {
 
         for (let k in data) {
             switch (k) {
-
+                case 'gravity_point':
+                case 'gravity_distance_scale':
+                case 'gravity':
+                case 'linear_damp':
+                case 'angular_damp': {
+                    this[k] = data[k];
+                } break;
+                case 'gravity_vec': {
+                    this[k].copy(data[k]);
+                } break;
             }
         }
 
