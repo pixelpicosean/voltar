668fac16e731cc527e4096715d4d40c7b1b59a12
Sean Bohan <pixelpicosean@gmail.com>
2018-10-29 21:53:52 +0800

    Import more Control properties

--- godot_importer/parser/res/Control.js
+++ godot_importer/parser/res/Control.js
@@ -1,4 +1,5 @@
 const {
+    int,
     real,
     boolean,
     Vector2,
@@ -20,6 +21,21 @@ module.exports = (data) => {
     res.margin_left = real(data.prop.margin_left);
     res.margin_top = real(data.prop.margin_top);
     res.margin_right = real(data.prop.margin_right);
+
+    res.grow_horizontal = int(data.prop.grow_horizontal);
+    res.grow_vertical = int(data.prop.grow_vertical);
+
+    res.rect_min_size = Vector2(data.prop.rect_min_size);
+    res.rect_rotation = real(data.prop.rect_rotation);
+    res.rect_scale = Vector2(data.prop.rect_scale);
+    res.rect_pivot_offset = Vector2(data.prop.rect_pivot_offset);
+    res.rect_clip_content = boolean(data.prop.rect_clip_content);
+
+    res.mouse_default_cursor_shape = int(data.prop.mouse_default_cursor_shape);
+
+    res.size_flags_horizontal = int(data.prop.size_flags_horizontal);
+    res.size_flags_vertical = int(data.prop.size_flags_vertical);
+    res.size_flags_stretch_ratio = real(data.prop.size_flags_stretch_ratio);
 
     return res;
 };
