9ebe2489c36d6e870cf6360b09b6c022c0a00ccc
Sean Bohan <pixelpicosean@gmail.com>
2018-11-01 10:02:56 +0800

    Add test of BitmapText to make sure we are not broken

--- src/game/test/Control.js
+++ src/game/test/Control.js
@@ -2,6 +2,8 @@ import * as v from 'engine/index';
 
 import TitleData from 'scene/Position.json';
 
+v.preload('media/Foo.fnt')
+
 export default class ControlTest extends v.Node2D {
     static instance() {
         const s = new ControlTest();
@@ -9,5 +11,10 @@ export default class ControlTest extends v.Node2D {
         return s;
     }
 
-    _enter_tree() { }
+    _enter_tree() {
+        this.add_child(new v.BitmapText('Hello', {
+            font: 'Foo',
+        }))
+        .set_position(100, 100);
+    }
 }
