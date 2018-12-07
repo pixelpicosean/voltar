d4d858925f60798b966a0bcbd48e175e229c2d0c
Sean Bohan <pixelpicosean@gmail.com>
2018-12-07 15:34:09 +0800

    Start to work on SAT collision solver

--- src/engine/servers/physics_2d/collision_solver_2d_sat.js
+++ src/engine/servers/physics_2d/collision_solver_2d_sat.js
@@ -1,6 +1,29 @@
 import { Shape2DSW } from "./shape_2d_sw";
 import { Matrix, Vector2 } from "engine/math/index";
 
+class _CollectorCallback2D {
+    constructor() {
+        /** @type {import("./collision_solver_2d_sw").CallbackResult} */
+        this.callback = null;
+        this.userdata = null;
+        this.swap = false;
+        this.collided = false;
+        this.normal = new Vector2();
+        this.sep_axis = new Vector2();
+    }
+    /**
+     * @param {Vector2} p_point_A
+     * @param {Vector2} p_point_B
+     */
+    call(p_point_A, p_point_B) {
+        if (this.swap) {
+            this.callback(p_point_B, p_point_A, this.userdata);
+        } else {
+            this.callback(p_point_A, p_point_B, this.userdata);
+        }
+    }
+}
+
 /**
  * @param {Shape2DSW} p_shape_A
  * @param {Matrix} p_transform_A
@@ -17,5 +40,8 @@ import { Matrix, Vector2 } from "engine/math/index";
  * @returns {boolean}
  */
 export function sat_2d_calculate_penetration(p_shape_A, p_transform_A, p_motion_A, p_shape_B, p_transform_B, p_motion_B, p_result_callback, p_userdata, p_swap = false, sep_axis = null, p_margin_A = 0, p_margin_B = 0) {
+    const type_A = p_shape_A.type;
+    const type_B = p_shape_B.type;
+
     return false;
 }
