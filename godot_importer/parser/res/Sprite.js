99bf8d085fd197081e6239686979efcc6770b070
Sean Bohan <pixelpicosean@gmail.com>
2019-01-20 13:14:55 +0800

    Import Sprite.offset as offset instead of pivot
    which will fix the offset issue after importing

--- godot_importer/parser/res/Sprite.js
+++ godot_importer/parser/res/Sprite.js
@@ -10,19 +10,13 @@ module.exports = (data) => {
     const res = Object.assign({}, Node2D(data), {
         type: 'Sprite',
         texture: Nullable(data.prop.texture),
-        pivot: Vector2(data.prop.offset),
+        offset: Vector2(data.prop.offset),
     });
 
     // Set anchor based on `center`
     const centered = boolean(data.prop.centered);
-    if (centered === undefined || (centered !== undefined && centered)) {
-        res.anchor = { x: 0.5, y: 0.5 };
-    }
-
-    // Revert pivot
-    if (res.pivot) {
-        res.pivot.x = -res.pivot.x;
-        res.pivot.y = -res.pivot.y;
+    if (centered !== undefined && !centered) {
+        res.anchor = { x: 0, y: 0 };
     }
 
     return res;
