c71c8e2c0bee2ac05a03b476bee9967b61877fb3
Sean Bohan <pixelpicosean@gmail.com>
2019-02-14 10:44:36 +0800

    Rewrite VObject with ES6

--- src/engine/dep/VObject.js
+++ src/engine/dep/VObject.js
@@ -1,55 +1,28 @@
-// @ts-nocheck
-const has = Object.prototype.hasOwnProperty;
-let prefix = '~';
-
-/**
- * Constructor to create a storage for our `EE` objects.
- * An `Events` instance is a plain object whose properties are event names.
- *
- * @constructor
- * @private
- */
-function Events() { }
-
-//
-// We try to not inherit from `Object.prototype`. In some engines creating an
-// instance in this way is faster than calling `Object.create(null)` directly.
-// If `Object.create(null)` is not supported we prefix the event names with a
-// character to make sure that the built-in object properties are not
-// overridden or used as an attack vector.
-//
-if (Object.create) {
-    Events.prototype = Object.create(null);
-
-    //
-    // This hack is needed because the `__proto__` property is still inherited in
-    // some old browsers like Android 4, iPhone 5.1, Opera 11 and Safari 5.
-    //
-    if (!new Events().__proto__) prefix = false;
-}
+import { remove_items } from "./index";
 
 /**
  * Representation of a single event listener.
- *
- * @param {Function} fn The listener function.
- * @param {*} context The context to invoke the listener with.
- * @param {Boolean} [once=false] Specify if the listener is a one-time listener.
- * @constructor
- * @private
  */
-function EE(fn, context, once) {
-    this.fn = fn;
-    this.context = context;
-    this.once = once || false;
+class EventListener {
+    /**
+     * @param {Function} fn The listener function.
+     * @param {any} context The context to invoke the listener with.
+     * @param {Boolean} [once=false] Specify if the listener is a one-time listener.
+     */
+    constructor(fn, context, once = false) {
+        this.fn = fn;
+        this.context = context;
+        this.once = once;
+    }
 }
 
 /**
  * Add a listener for a given event.
  *
  * @param {VObject} emitter Reference to the `VObject` instance.
- * @param {(String|Symbol)} event The event name.
+ * @param {string|Symbol} event The event name.
  * @param {Function} fn The listener function.
- * @param {*} context The context to invoke the listener with.
+ * @param {any} context The context to invoke the listener with.
  * @param {Boolean} once Specify if the listener is a one-time listener.
  * @returns {VObject}
  * @private
@@ -59,286 +32,171 @@ function add_listener(emitter, event, fn, context, once) {
         throw new TypeError('The listener must be a function');
     }
 
-    var listener = new EE(fn, context || emitter, once)
-        , evt = prefix ? prefix + event : event;
+    const listener = new EventListener(fn, context || emitter, once);
 
-    if (!emitter._events[evt]) emitter._events[evt] = listener, emitter._eventsCount++;
-    else if (!emitter._events[evt].fn) emitter._events[evt].push(listener);
-    else emitter._events[evt] = [emitter._events[evt], listener];
+    let listeners = emitter._events.get(event);
+    if (!listeners) {
+        listeners = [];
+        emitter._events.set(event, listeners);
+    }
+
+    listeners.push(listener);
 
     return emitter;
 }
 
 /**
- * Clear event by name.
- *
- * @param {VObject} emitter Reference to the `VObject` instance.
- * @param {(String|Symbol)} evt The Event name.
- * @private
- */
-function clear_event(emitter, evt) {
-    if (--emitter._eventsCount === 0) emitter._events = new Events();
-    else delete emitter._events[evt];
-}
-
-/**
  * Base class of most engine classes, with ability to emit events.
- *
- * @constructor
- * @public
  */
-function VObject() {
-    this._events = new Events();
-    this._eventsCount = 0;
-}
-
-/**
- * Return an array listing the events for which the emitter has registered
- * listeners.
- *
- * @returns {Array}
- * @public
- */
-VObject.prototype.get_signal_list = function get_signal_list() {
-    var names = []
-        , events
-        , name;
-
-    if (this._eventsCount === 0) return names;
-
-    for (name in (events = this._events)) {
-        if (has.call(events, name)) names.push(prefix ? name.slice(1) : name);
+export default class VObject {
+    constructor() {
+        /**
+         * @type {Map<string|Symbol, EventListener[]>}
+         */
+        this._events = new Map();
     }
 
-    if (Object.getOwnPropertySymbols) {
-        return names.concat(Object.getOwnPropertySymbols(events));
+    /**
+     * Return an array listing the events for which the emitter has registered
+     * listeners.
+     */
+    get_signal_list() {
+        return this._events.keys();
     }
 
-    return names;
-};
-
-/**
- * Return the listeners registered for a given event.
- *
- * @param {(String|Symbol)} event The event name.
- * @returns {Array} The registered listeners.
- * @public
- */
-VObject.prototype.get_signal_connection_list = function listget_signal_connection_listeners(event) {
-    var evt = prefix ? prefix + event : event
-        , handlers = this._events[evt];
-
-    if (!handlers) return [];
-    if (handlers.fn) return [handlers.fn];
-
-    for (var i = 0, l = handlers.length, ee = new Array(l); i < l; i++) {
-        ee[i] = handlers[i].fn;
+    /**
+     * Return the listeners registered for a given event.
+     *
+     * @param {string|Symbol} event The event name.
+     */
+    get_signal_connection_listeners(event) {
+        return this._events.get(event);
     }
 
-    return ee;
-};
+    /**
+     * Return the number of listeners listening to a given event.
+     *
+     * @param {string|Symbol} event The event name.
+     */
+    get_signal_connection_count(event) {
+        const listeners = this._events.get(event);
 
-/**
- * Return the number of listeners listening to a given event.
- *
- * @param {(String|Symbol)} event The event name.
- * @returns {Number} The number of listeners.
- * @public
- */
-VObject.prototype.get_signal_connection_count = function get_signal_connection_count(event) {
-    var evt = prefix ? prefix + event : event
-        , listeners = this._events[evt];
-
-    if (!listeners) return 0;
-    if (listeners.fn) return 1;
-    return listeners.length;
-};
-
-/**
- * Calls each of the listeners registered for a given event.
- *
- * @param {(String|Symbol)} event The event name.
- * @param {any} [a1]
- * @param {any} [a2]
- * @param {any} [a3]
- * @param {any} [a4]
- * @param {any} [a5]
- * @returns {Boolean} `true` if the event had listeners, else `false`.
- * @public
- */
-VObject.prototype.emit_signal = function emit_signal(event, a1, a2, a3, a4, a5) {
-    var evt = prefix ? prefix + event : event;
-
-    if (!this._events[evt]) return false;
-
-    var listeners = this._events[evt]
-        , len = arguments.length
-        , args
-        , i;
-
-    if (listeners.fn) {
-        if (listeners.once) this.disconnect(event, listeners.fn, undefined, true);
-
-        switch (len) {
-            case 1: return listeners.fn.call(listeners.context), true;
-            case 2: return listeners.fn.call(listeners.context, a1), true;
-            case 3: return listeners.fn.call(listeners.context, a1, a2), true;
-            case 4: return listeners.fn.call(listeners.context, a1, a2, a3), true;
-            case 5: return listeners.fn.call(listeners.context, a1, a2, a3, a4), true;
-            case 6: return listeners.fn.call(listeners.context, a1, a2, a3, a4, a5), true;
+        if (!listeners) {
+            return 0;
         }
 
-        for (i = 1, args = new Array(len - 1); i < len; i++) {
-            args[i - 1] = arguments[i];
+        return listeners.length;
+    }
+
+    /**
+     * Calls each of the listeners registered for a given event.
+     *
+     * @param {string|Symbol} event The event name.
+     * @param {any} [args]
+     */
+    emit_signal(event, ...args) {
+        const listeners = this._events.get(event);
+
+        if (!listeners) {
+            return false;
         }
 
-        listeners.fn.apply(listeners.context, args);
-    } else {
-        var length = listeners.length
-            , j;
+        for (const e  of listeners) {
+            e.fn.call(e.context, ...args);
+        }
 
-        for (i = 0; i < length; i++) {
-            if (listeners[i].once) this.disconnect(event, listeners[i].fn, undefined, true);
+        return true;
+    }
 
-            switch (len) {
-                case 1: listeners[i].fn.call(listeners[i].context); break;
-                case 2: listeners[i].fn.call(listeners[i].context, a1); break;
-                case 3: listeners[i].fn.call(listeners[i].context, a1, a2); break;
-                case 4: listeners[i].fn.call(listeners[i].context, a1, a2, a3); break;
-                default:
-                    if (!args) for (j = 1, args = new Array(len - 1); j < len; j++) {
-                        args[j - 1] = arguments[j];
-                    }
+    /**
+     * Add a listener for a given event.
+     *
+     * @param {string|Symbol} event The event name.
+     * @param {Function} fn The listener function.
+     * @param {any} [context=this] The context to invoke the listener with.
+     */
+    connect(event, fn, context) {
+        return add_listener(this, event, fn, context || this, false);
+    }
 
-                    listeners[i].fn.apply(listeners[i].context, args);
+    /**
+     * Add a one-time listener for a given event.
+     *
+     * @param {string|Symbol} event The event name.
+     * @param {Function} fn The listener function.
+     * @param {any} [context=this] The context to invoke the listener with.
+     */
+    connect_once(event, fn, context) {
+        return add_listener(this, event, fn, context || this, true);
+    }
+
+    /**
+     * Remove the listeners of a given event.
+     *
+     * @param {string|Symbol} event The event name.
+     * @param {Function} fn Only remove the listeners that match this function.
+     * @param {any} [context] Only remove the listeners that have this context.
+     * @param {Boolean} [once] Only remove one-time listeners.
+     */
+    disconnect(event, fn, context = undefined, once = undefined) {
+        const listeners = this._events.get(event);
+
+        if (!listeners) {
+            return this;
+        }
+
+        for (let i = listeners.length - 1; i >= 0; i--) {
+            const e = listeners[i];
+            if (
+                e.fn === fn
+                &&
+                (context === undefined || (context !== undefined && e.context === context))
+                &&
+                (once === undefined || (once !== undefined && e.once === once))
+            ) {
+                remove_items(listeners, i, 1);
             }
         }
-    }
 
-    return true;
-};
-
-/**
- * Add a listener for a given event.
- *
- * @param {(String|Symbol)} event The event name.
- * @param {Function} fn The listener function.
- * @param {*} [context=this] The context to invoke the listener with.
- * @returns {VObject} `this`.
- * @public
- */
-VObject.prototype.connect = function connect(event, fn, context) {
-    return add_listener(this, event, fn, context, false);
-};
-
-/**
- * Add a one-time listener for a given event.
- *
- * @param {(String|Symbol)} event The event name.
- * @param {Function} fn The listener function.
- * @param {*} [context=this] The context to invoke the listener with.
- * @returns {VObject} `this`.
- * @public
- */
-VObject.prototype.connect_once = function connect_once(event, fn, context) {
-    return add_listener(this, event, fn, context, true);
-};
-
-/**
- * Remove the listeners of a given event.
- *
- * @param {(String|Symbol)} event The event name.
- * @param {Function} fn Only remove the listeners that match this function.
- * @param {*} [context] Only remove the listeners that have this context.
- * @param {Boolean} [once] Only remove one-time listeners.
- * @returns {VObject} `this`.
- * @public
- */
-VObject.prototype.disconnect = function disconnect(event, fn, context, once) {
-    var evt = prefix ? prefix + event : event;
-
-    if (!this._events[evt]) return this;
-    if (!fn) {
-        clear_event(this, evt);
         return this;
     }
 
-    var listeners = this._events[evt];
-
-    if (listeners.fn) {
-        if (
-            listeners.fn === fn &&
-            (!once || listeners.once) &&
-            (!context || listeners.context === context)
-        ) {
-            clear_event(this, evt);
+    /**
+     * Whether an function(with context) is connected to this object.
+     *
+     * @param {string|Symbol} event The event name.
+     * @param {Function} fn
+     * @param {any} [context]
+     * @returns {boolean}
+     */
+    is_connected(event, fn, context) {
+        const listeners = this._events.get(event);
+        if (!listeners) {
+            return false;
         }
-    } else {
-        for (var i = 0, events = [], length = listeners.length; i < length; i++) {
+
+        for (let e of listeners) {
             if (
-                listeners[i].fn !== fn ||
-                (once && !listeners[i].once) ||
-                (context && listeners[i].context !== context)
+                (fn === e.fn)
+                &&
+                (context === undefined || (context !== undefined && context === e.context))
             ) {
-                events.push(listeners[i]);
+                return true;
             }
         }
 
-        //
-        // Reset the array, or remove it completely if we have no more listeners.
-        //
-        if (events.length) this._events[evt] = events.length === 1 ? events[0] : events;
-        else clear_event(this, evt);
+        return false;
     }
 
-    return this;
-};
+    /**
+     * Remove all listeners, or those of the specified event.
+     *
+     * @param {string|Symbol} [event] The event name.
+     * @returns {VObject} `this`.
+     */
+    disconnect_all(event) {
+        this._events.delete(event);
 
-/**
- * Whether an function(with context) is connected to this object.
- *
- * @param {(String|Symbol)} event The event name.
- * @param {Function} fn
- * @param {any} [context]
- * @returns {boolean}
- * @public
- */
-VObject.prototype.is_connected = function is_connected(event, fn, context) {
-    var evt = prefix ? prefix + event : event;
-
-    if (!this._events[evt]) return false;
-    if (!fn) return false;
-
-    var listeners = this._events[evt];
-
-    return (
-        listeners.fn === fn
-        &&
-        listeners.context === context
-    );
-};
-
-/**
- * Remove all listeners, or those of the specified event.
- *
- * @param {(String|Symbol)} [event] The event name.
- * @returns {VObject} `this`.
- * @public
- */
-VObject.prototype.disconnect_all = function disconnect_all(event) {
-    var evt;
-
-    if (event) {
-        evt = prefix ? prefix + event : event;
-        if (this._events[evt]) clear_event(this, evt);
-    } else {
-        this._events = new Events();
-        this._eventsCount = 0;
+        return this;
     }
-
-    return this;
-};
-
-export const prefixed = prefix;
-
-export default VObject;
+}
