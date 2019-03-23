4237a3764d36443e130f8cd27ec3e26981097499
Sean Bohan <pixelpicosean@gmail.com>
2019-03-23 19:36:12 +0800

    Fix shuffle util function

--- src/engine/utils/index.js
+++ src/engine/utils/index.js
@@ -231,11 +231,11 @@ export function shuffle(array) {
     const len = array.length - 1;
 
     for (let i = len; i > 0; i--) {
-        const randomIndex = Math.floor(this.frac() * (i + 1));
-        const itemAtIndex = array[randomIndex];
+        const random_index = Math.floor(Math.random() * (i + 1));
+        const item_at_index = array[random_index];
 
-        array[randomIndex] = array[i];
-        array[i] = itemAtIndex;
+        array[random_index] = array[i];
+        array[i] = item_at_index;
     }
 
     return array;
