8174e18c6c2d31e716237cdaa334e4e1c3581c4a
Sean Bohan <pixelpicosean@gmail.com>
2019-03-25 07:18:31 +0800

    Support Symbol as the event key yield function

--- src/engine/core/yield.js
+++ src/engine/core/yield.js
@@ -2,7 +2,7 @@ import VObject from "engine/core/v_object";
 
 /**
  * @param {VObject} obj
- * @param {string} event
+ * @param {string | Symbol} event
  */
 export default function(obj, event) {
     return new Promise(res => obj.connect_once(event, res));
