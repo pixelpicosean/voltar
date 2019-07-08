1bde6f9a81485728402678ab121ac7c0d90a2e01
Sean Bohan <pixelpicosean@gmail.com>
2019-07-08 12:43:40 +0800

    Do not change "rect_scale" to "scale" while converting

--- godot_importer/parser/registry.js
+++ godot_importer/parser/registry.js
@@ -1,7 +1,4 @@
 module.exports.path_modifiers = [
-    // Control
-    (str) => str.replace(/rect_scale/g, 'scale'),
-
     // Label
     (str) => str.replace(/custom_colors\/font_color/g, 'fill'),
 ];
