cb2e18e7156f86700b927d13e6f6f6be131fa7de
Sean Bohan <pixelpicosean@gmail.com>
2019-09-22 10:13:22 +0800

    Add setter/getter back to sprite, which is required by AnimationPlayer

--- src/engine/scene/2d/sprite.js
+++ src/engine/scene/2d/sprite.js
@@ -15,171 +15,70 @@ const white = new Color(1, 1, 1);
 export class Sprite extends Node2D {
     get class() { return 'Sprite' }
 
-    /**
-     * @param {boolean} p_center
-     */
-    set_centered(p_center) {
-        this.centered = p_center;
-        this.update();
-        this.item_rect_changed();
+    get texture() { return this._texture }
+    set texture(value) { this.set_texture(value) }
+
+    get normal_map() { return this._normal_map }
+    set normal_map(value) { this.set_normal_map(value) }
+
+    get centered() { return this._centered }
+    set centered(value) { this.set_centered(value) }
+
+    get offset() { return this._offset }
+    set offset(value) { this.set_offset(value) }
+
+    get flip_h() { return this._flip_h }
+    set flip_h(value) { this.set_flip_h(value) }
+
+    get flip_v() { return this._flip_v }
+    set flip_v(value) { this.set_flip_v(value) }
+
+    get region_enabled() { return this._region_enabled }
+    set region_enabled(value) { this.set_region_enabled(value) }
+
+    get region_rect() { return this._region_rect }
+    set region_rect(value) { this.set_region_rect(value) }
+
+    get region_filter_clip() { return this._region_filter_clip }
+    set region_filter_clip(value) { this.set_region_filter_clip(value) }
+
+    get frame() { return this._frame }
+    set frame(value) { this.set_frame(value) }
+
+    get hframes() { return this._hframes }
+    set hframes(value) { this.set_hframes(value) }
+
+    get vframes() { return this._vframes }
+    set vframes(value) { this.set_vframes(value) }
+
+    get frame_coords() {
+        return Vector2.new(this._frame % this._hframes, this._frame / this._hframes).floor();
     }
-
-    /**
-     * @param {Vector2Like} p_offset
-     */
-    set_offset(p_offset) {
-        this.offset.copy(p_offset);
-        this.update();
-        this.item_rect_changed();
-    }
-    /**
-     * @param {number} p_offset_x
-     * @param {number} p_offset_y
-     */
-    set_offset_n(p_offset_x, p_offset_y) {
-        this.offset.set(p_offset_x, p_offset_y);
-        this.update();
-        this.item_rect_changed();
-    }
-
-    /**
-     * @param {boolean} p_flip
-     */
-    set_flip_h(p_flip) {
-        this.flip_h = p_flip;
-        this.update();
-    }
-    /**
-     * @param {boolean} p_flip
-     */
-    set_flip_v(p_flip) {
-        this.flip_v = p_flip;
-        this.update();
-    }
-
-    /**
-     * @param {boolean} p_enabled
-     */
-    set_region_enabled(p_enabled) {
-        if (this.region_enabled) return;
-        this.region_enabled = p_enabled;
-        this.update();
-    }
-
-    /**
-     * @param {Rect2} p_rect
-     */
-    set_region_rect(p_rect) {
-        if (this.region_rect.equals(p_rect)) return;
-        this.region_rect.copy(p_rect);
-        if (this.region_enabled) {
-            this.item_rect_changed();
-        }
-    }
-
-    /**
-     * @param {boolean} p_enabled
-     */
-    set_region_filter_clip(p_enabled) {
-        this.region_filter_clip = p_enabled;
-        this.update();
-    }
-
-    /**
-     * @param {number} p_frame
-     */
-    set_frame(p_frame) {
-        if (this.frame === p_frame) {
-            this.item_rect_changed();
-        }
-
-        this.frame = p_frame;
-
-        this.emit_signal('frame_changed');
-    }
-
-    /**
-     * @param {Vector2Like} p_coord
-     */
-    set_frame_coords(p_coord) {
-        this.set_frame(Math.floor(p_coord.y * this.hframes + p_coord.x));
-    }
-
-    get_frame_coords() {
-        return Vector2.new(this.frame % this.hframes, this.frame / this.hframes).floor();
-    }
-
-    /**
-     * @param {number} p_amount
-     */
-    set_vframes(p_amount) {
-        this.vframes = p_amount;
-        this.update();
-        this.item_rect_changed();
-    }
-    /**
-     * @param {number} p_amount
-     */
-    set_hframes(p_amount) {
-        this.hframes = p_amount;
-        this.update();
-        this.item_rect_changed();
-    }
-
-    get_rect() {
-        if (!this.texture || !this.texture.texture) {
-            return Rect2.new(0, 0, 1, 1);
-        }
-
-        const s = Vector2.new(0, 0);
-
-        if (this.region_enabled) {
-            s.set(this.region_rect.width, this.region_rect.height);
-        } else {
-            s.set(this.texture.get_width(), this.texture.get_height());
-        }
-
-        s.x /= this.hframes;
-        s.y /= this.vframes;
-
-        const ofs = Vector2.new(0, 0);
-        if (this.centered) {
-            ofs.x -= s.x / 2;
-            ofs.y -= s.y / 2;
-        }
-        if (s.is_zero()) {
-            s.set(1, 1);
-        }
-
-        const rect = Rect2.new(ofs.x, ofs.y, s.x, s.y);
-        Vector2.free(s);
-        Vector2.free(ofs);
-
-        return rect;
+    set frame_coords(p_coord) {
+        this.set_frame(Math.floor(p_coord.y * this._hframes + p_coord.x));
     }
 
     constructor() {
         super();
 
-        this.centered = true;
-        this.offset = new Vector2(0, 0);
-        this.flip_h = false;
-        this.flip_v = false;
-        this.region_enabled = false;
-        this.region_filter_clip = false;
-        this.region_rect = new Rect2();
+        this._centered = true;
+        this._offset = new Vector2(0, 0);
+        this._flip_h = false;
+        this._flip_v = false;
+        this._region_enabled = false;
+        this._region_filter_clip = false;
+        this._region_rect = new Rect2();
 
-        this.frame = 0;
-        this.frame_coords = new Vector2();
+        this._frame = 0;
 
-        this.hframes = 1;
-        this.vframes = 1;
-
-        /** @type {ImageTexture} */
-        this.texture = null;
+        this._hframes = 1;
+        this._vframes = 1;
 
         /** @type {ImageTexture} */
-        this.normal_map = null;
+        this._texture = null;
+
+        /** @type {ImageTexture} */
+        this._normal_map = null;
     }
 
     /* virtual */
@@ -212,7 +111,7 @@ export class Sprite extends Node2D {
     _notification(p_what) {
         switch (p_what) {
             case NOTIFICATION_DRAW: {
-                if (!this.texture) {
+                if (!this._texture) {
                     break;
                 }
 
@@ -220,7 +119,7 @@ export class Sprite extends Node2D {
                 const dst_rect = Rect2.new();
 
                 const filter_clip = this._get_rects(src_rect, dst_rect);
-                this.texture.draw_rect_region(this.canvas_item, dst_rect, src_rect, white, false, this.normal_map, filter_clip);
+                this._texture.draw_rect_region(this.canvas_item, dst_rect, src_rect, white, false, this._normal_map, filter_clip);
 
                 Rect2.free(src_rect);
                 Rect2.free(dst_rect);
@@ -237,8 +136,8 @@ export class Sprite extends Node2D {
         /** @type {ImageTexture} */
         const texture = (typeof(p_texture) === 'string') ? resource_map[p_texture] : p_texture;
 
-        if (this.texture === texture) return;
-        this.texture = texture;
+        if (this._texture === texture) return;
+        this._texture = texture;
         this.update();
         this.item_rect_changed();
     }
@@ -246,8 +145,155 @@ export class Sprite extends Node2D {
      * @param {ImageTexture} p_texture
      */
     set_normal_map(p_texture) {
-        this.normal_map = p_texture;
+        this._normal_map = p_texture;
         this.update();
+    }
+
+    /**
+     * @param {boolean} p_center
+     */
+    set_centered(p_center) {
+        this._centered = p_center;
+        this.update();
+        this.item_rect_changed();
+    }
+
+    /**
+     * @param {Vector2Like} p_offset
+     */
+    set_offset(p_offset) {
+        this._offset.copy(p_offset);
+        this.update();
+        this.item_rect_changed();
+    }
+    /**
+     * @param {number} p_offset_x
+     * @param {number} p_offset_y
+     */
+    set_offset_n(p_offset_x, p_offset_y) {
+        this._offset.set(p_offset_x, p_offset_y);
+        this.update();
+        this.item_rect_changed();
+    }
+
+    /**
+     * @param {boolean} p_flip
+     */
+    set_flip_h(p_flip) {
+        this._flip_h = p_flip;
+        this.update();
+    }
+    /**
+     * @param {boolean} p_flip
+     */
+    set_flip_v(p_flip) {
+        this._flip_v = p_flip;
+        this.update();
+    }
+
+    /**
+     * @param {number} p_amount
+     */
+    set_vframes(p_amount) {
+        this._vframes = p_amount;
+        this.update();
+        this.item_rect_changed();
+    }
+    /**
+     * @param {number} p_amount
+     */
+    set_hframes(p_amount) {
+        this._hframes = p_amount;
+        this.update();
+        this.item_rect_changed();
+    }
+
+    get_rect() {
+        if (!this._texture || !this._texture.texture) {
+            return Rect2.new(0, 0, 1, 1);
+        }
+
+        const s = Vector2.new(0, 0);
+
+        if (this._region_enabled) {
+            s.set(this._region_rect.width, this._region_rect.height);
+        } else {
+            s.set(this._texture.get_width(), this._texture.get_height());
+        }
+
+        s.x /= this._hframes;
+        s.y /= this._vframes;
+
+        const ofs = Vector2.new(0, 0);
+        if (this._centered) {
+            ofs.x -= s.x / 2;
+            ofs.y -= s.y / 2;
+        }
+        if (s.is_zero()) {
+            s.set(1, 1);
+        }
+
+        const rect = Rect2.new(ofs.x, ofs.y, s.x, s.y);
+        Vector2.free(s);
+        Vector2.free(ofs);
+
+        return rect;
+    }
+
+    /**
+     * @param {boolean} p_enabled
+     */
+    set_region_enabled(p_enabled) {
+        if (this._region_enabled) return;
+        this._region_enabled = p_enabled;
+        this.update();
+    }
+
+    /**
+     * @param {Rect2} p_rect
+     */
+    set_region_rect(p_rect) {
+        if (this._region_rect.equals(p_rect)) return;
+        this._region_rect.copy(p_rect);
+        if (this._region_enabled) {
+            this.item_rect_changed();
+        }
+    }
+
+    /**
+     * @param {boolean} p_enabled
+     */
+    set_region_filter_clip(p_enabled) {
+        this._region_filter_clip = p_enabled;
+        this.update();
+    }
+
+    /**
+     * @param {number} p_frame
+     */
+    set_frame(p_frame) {
+        if (this._frame === p_frame) {
+            this.item_rect_changed();
+        }
+
+        this._frame = p_frame;
+
+        this.emit_signal('frame_changed');
+    }
+
+    /**
+     * @param {Vector2Like} p_coord
+     */
+    set_frame_coords(p_coord) {
+        this.set_frame(Math.floor(p_coord.y * this._hframes + p_coord.x));
+    }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     */
+    set_frame_coords_n(x, y) {
+        this.set_frame(Math.floor(y * this._hframes + x));
     }
 
     /* private */
@@ -261,17 +307,17 @@ export class Sprite extends Node2D {
 
         let r_filter_clip = false;
 
-        if (this.region_enabled) {
-            r_filter_clip = this.region_filter_clip;
-            base_rect.copy(this.region_rect);
+        if (this._region_enabled) {
+            r_filter_clip = this._region_filter_clip;
+            base_rect.copy(this._region_rect);
         } else {
             r_filter_clip = false;
-            base_rect.width = this.texture.get_width();
-            base_rect.height = this.texture.get_height();
+            base_rect.width = this._texture.get_width();
+            base_rect.height = this._texture.get_height();
         }
 
-        const frame_size = Vector2.new(base_rect.width / this.hframes, base_rect.height / this.vframes);
-        const frame_offset = Vector2.new(this.frame % this.hframes, this.frame / this.hframes);
+        const frame_size = Vector2.new(base_rect.width / this._hframes, base_rect.height / this._vframes);
+        const frame_offset = Vector2.new(this._frame % this._hframes, this._frame / this._hframes);
         frame_offset.multiply(frame_size);
 
         r_src_rect.width = frame_size.x;
@@ -279,8 +325,8 @@ export class Sprite extends Node2D {
         r_src_rect.x = base_rect.x + frame_offset.x;
         r_src_rect.y = base_rect.y + frame_offset.y;
 
-        const dest_offset = this.offset.clone();
-        if (this.centered) {
+        const dest_offset = this._offset.clone();
+        if (this._centered) {
             dest_offset.x -= frame_size.x / 2;
             dest_offset.y -= frame_size.y / 2;
         }
@@ -290,10 +336,10 @@ export class Sprite extends Node2D {
 
         r_dst_rect.set(dest_offset.x, dest_offset.y, frame_size.x, frame_size.y);
 
-        if (this.flip_h) {
+        if (this._flip_h) {
             r_dst_rect.width = -r_dst_rect.width;
         }
-        if (this.flip_v) {
+        if (this._flip_v) {
             r_dst_rect.height = - r_dst_rect.height;
         }
 
