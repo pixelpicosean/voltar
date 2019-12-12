2e7d8311b348b0d0678678b6eb0ed85e76f48c75
Sean Bohan <pixelpicosean@gmail.com>
2019-12-13 07:22:55 +0800

    Cleanup imported tile_set data

--- tscn/converter/res/TileSet.js
+++ tscn/converter/res/TileSet.js
@@ -1,9 +1,32 @@
+const _ = require('lodash');
+
 module.exports = (data) => {
-    return {
+    const tileset = {
         id: data.attr.id,
         type: 'TileSet',
         resource: data.prop,
     }
+
+    tileset.resource.forEach((tile) => {
+        for (const k in tile) {
+            tile.autotile = tile.autotile || {};
+            if (k.startsWith('autotile/')) {
+                const prop_key = k.replace('autotile/', '');
+                if (!_.isEmpty(tile[k])) {
+                    tile.autotile[prop_key] = tile[k];
+                }
+                delete tile[k];
+            }
+        }
+
+        for (const k in tile) {
+            if (_.isObjectLike(tile[k]) && _.isEmpty(tile[k])) {
+                tile[k] = undefined;
+            }
+        }
+    })
+
+    return tileset;
 };
 
 module.exports.is_tres = true;
