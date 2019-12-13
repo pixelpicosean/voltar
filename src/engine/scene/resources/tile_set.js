b6931d8a2e6db230f520ca7820d18ee1371a09e9
Sean Bohan <pixelpicosean@gmail.com>
2019-12-13 21:21:54 +0800

    Fix tile_set shape warning

--- src/engine/scene/resources/tile_set.js
+++ src/engine/scene/resources/tile_set.js
@@ -33,13 +33,9 @@ class ShapeData {
         if (data.shape_transform !== undefined) {
             this.shape_transform.from_array(data.shape_transform);
         }
-        if (data.shape !== undefined) {
-            if (data.shape.type === 'ConvexPolygonShape2D') {
-                this.shape = new ConvexPolygonShape2D;
-                /** @type {ConvexPolygonShape2D} */(this.shape).set_points_in_pool_vec2(data.shape.points);
-            } else {
-                console.warn(`${data.shape.type} shape in TileSet is not supported yet!`);
-            }
+        if (data.shape !== undefined && data.shape.type >= 0) {
+            this.shape = new ConvexPolygonShape2D;
+            /** @type {ConvexPolygonShape2D} */(this.shape).set_points_in_pool_vec2(data.shape.points);
         }
 
         return this;
