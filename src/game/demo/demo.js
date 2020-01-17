c60da5d76236527af3ae5c4d218856e43bc7c930
Sean Bohan <pixelpicosean@gmail.com>
2020-01-18 06:37:28 +0800

    Cleanup demo scene and unnecessary images

--- src/game/demo/demo.js
+++ src/game/demo/demo.js
@@ -1,13 +1,20 @@
 import * as v from 'engine/index';
 
-
-export class MainScene extends v.Node {
+export class MainScene extends v.Node2D {
     static instance() { return new MainScene }
 
-    constructor() {
-        super();
+    _ready() {
+        this.update();
+    }
+    _draw() {
+        this.draw_polyline([
+            40, 0,
+            40, 80,
+            80, 80,
+            // 120, 120,
+        ], { r: 1, g: 1, b: 1, a: 1 }, 20);
+        // this.draw_rect(new v.Rect2(0, 0, 20, 20), new v.Color(0, 1, 1, 1), true);
     }
 }
-v.GDCLASS(MainScene, v.Node)
 
 v.attach_script('res://scene/demo.tscn', MainScene);
