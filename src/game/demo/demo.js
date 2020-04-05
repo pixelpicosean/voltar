d97c62706705c2a6c967768d3b36fa7d39b938c7
Sean Bohan <pixelpicosean@gmail.com>
2020-04-05 20:12:45 +0800

    Demo with shader test

--- src/game/demo/demo.js
+++ src/game/demo/demo.js
@@ -3,7 +3,9 @@ import * as v from 'engine/index';
 export class MainScene extends v.Node2D {
     static instance() { return new MainScene }
 
-    _ready() { }
+    _ready() {
+        const sprite = this.get_node("shader");
+    }
 }
 
 v.attach_script('res://scene/demo.tscn', MainScene);
