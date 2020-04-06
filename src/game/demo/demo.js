8f429e6ef973cf7b4b63228f6d4ab89932d48b4c
Sean Bohan <pixelpicosean@gmail.com>
2020-04-06 15:13:51 +0800

    Postprocessing example using screen texture shader

--- src/game/demo/demo.js
+++ src/game/demo/demo.js
@@ -3,9 +3,7 @@ import * as v from 'engine/index';
 export class MainScene extends v.Node2D {
     static instance() { return new MainScene }
 
-    _ready() {
-        const sprite = this.get_node("shader");
-    }
+    _ready() { }
 }
 
 v.attach_script('res://scene/demo.tscn', MainScene);
