bf48036c674ff00b1b46d4426b49fa3978849913
Sean Bohan <pixelpicosean@gmail.com>
2020-04-21 17:32:36 +0800

    Demo shows perspective 3D scene rendering

--- src/game/demo/demo.js
+++ src/game/demo/demo.js
@@ -1,9 +1,16 @@
 import * as v from 'engine/index';
 
-export class MainScene extends v.Control {
+export class MainScene extends v.Spatial {
     static instance() { return new MainScene }
 
-    _ready() { }
+    _ready() {
+        this.mesh = this.get_node("mesh");
+
+        this.set_process(true);
+    }
+    _process(delta) {
+        this.mesh.rotate_y(Math.PI * 0.4 * delta);
+    }
 }
 
-v.attach_script('res://scene/demo.tscn', MainScene);
+v.attach_script('res://scene/demo_3d.tscn', MainScene);
