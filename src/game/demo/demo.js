49168d51147dab9a615a2fe57a93fc562e17a920
Sean Bohan <pixelpicosean@gmail.com>
2020-05-09 19:30:34 +0800

    Update 3D demo

--- src/game/demo/demo.js
+++ src/game/demo/demo.js
@@ -3,10 +3,10 @@ import * as v from 'engine/index';
 export class MainScene extends v.Spatial {
     static instance() { return new MainScene }
 
-    _ready() {
+    async _ready() {
         this.mesh = this.get_node("cube");
 
-        // this.set_process(true);
+        this.set_process(true);
     }
     _process(delta) {
         this.mesh.rotate_y(Math.PI * 0.4 * delta);
