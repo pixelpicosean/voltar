1a966229c44701331b0585f443c1a36c7b5224a6
Sean Bohan <pixelpicosean@gmail.com>
2020-05-11 22:11:52 +0800

    Import Suzanne for test

--- src/game/demo/demo.js
+++ src/game/demo/demo.js
@@ -4,11 +4,13 @@ export class MainScene extends v.Spatial {
     static instance() { return new MainScene }
 
     async _ready() {
+        this.bot = this.get_node("ebox");
         this.mesh = this.get_node("cube");
 
         this.set_process(true);
     }
     _process(delta) {
+        this.bot.rotate_y(-Math.PI * 0.3 * delta);
         this.mesh.rotate_y(Math.PI * 0.4 * delta);
     }
 }
