e92db3ea9188cdc790dbdb3d1a47044a58b9140e
Sean Bohan <pixelpicosean@gmail.com>
2020-06-08 14:01:31 +0800

    Fix converting of animations with packed transform tracks

--- tscn/converter/res/Animation.js
+++ tscn/converter/res/Animation.js
@@ -20,32 +20,67 @@ module.exports = (data) => {
     // Support up to 128 tracks per animation
     for (let i = 0; i < 128; i++) {
         let track_pre = `tracks/${i}`;
+        let track_type = data.prop[`${track_pre}/type`];
 
-        if (!_.has(data.prop, `${track_pre}/type`)) {
+        if (!track_type) {
             break;
         }
 
         let keys_data = data.prop[`${track_pre}/keys`];
-        if (!keys_data.values) {
-            console.log('does not have values');
+
+        let times = [];
+        let transitions = [];
+        let values = [];
+
+        let packed_transform = false;
+        if (track_type === 'transform') {
+            for (let i = 0; i < keys_data.length; i += 12) {
+                times.push(keys_data[i + 0]);
+                transitions.push(keys_data[i + 1]);
+                values.push([
+                    // location
+                    keys_data[i + 2],
+                    keys_data[i + 3],
+                    keys_data[i + 4],
+                    // rotation
+                    keys_data[i + 5],
+                    keys_data[i + 6],
+                    keys_data[i + 7],
+                    keys_data[i + 8],
+                    // scale
+                    keys_data[i + 9],
+                    keys_data[i + 10],
+                    keys_data[i + 11],
+                ])
+            }
+            packed_transform = true;
+        } else if (track_type === 'method') {
+            times = PoolRealArray(keys_data.times);
+            transitions = PoolRealArray(keys_data.transitions);
+            values = MethodArray(keys_data.values);
+        } else {
+            times = PoolRealArray(keys_data.times);
+            transitions = PoolRealArray(keys_data.transitions);
+            values = GeneralArray(keys_data.values);
         }
-        let track_type = data.prop[`${track_pre}/type`];
+
         let track = {
             type: track_type,
             path: NodePath(data.prop[`${track_pre}/path`]),
             interp: data.prop[`${track_pre}/interp`],
             loop_wrap: data.prop[`${track_pre}/loop_wrap`],
-            // imported: boolean(data.prop[`${track_pre}/imported`]),
             enabled: data.prop[`${track_pre}/enabled`],
             keys: {
-                times: PoolRealArray(keys_data.times),
-                transitions: PoolRealArray(keys_data.transitions),
+                times,
+                transitions,
                 update: keys_data.update,
-                values: (track_type === 'method') ? MethodArray(keys_data.values) : GeneralArray(keys_data.values),
+                values,
             },
         };
 
-        if (track.keys.values.length && track.keys.values[0].length === 12) {
+        if (track_type === 'transform' && packed_transform) {
+            track.value_type = "PackedTransform";
+        } else if (track_type === 'value' && Array.isArray(track.keys.values[0]) && track.keys.values[0].length === 12) {
             track.value_type = "Transform";
         }
 
