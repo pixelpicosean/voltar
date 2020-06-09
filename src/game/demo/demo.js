d567bc8d28a78a086328b853c1c90fa3e4572354
Sean Bohan <pixelpicosean@gmail.com>
2020-06-09 21:14:00 +0800

    Update 3D demo

--- src/game/demo/demo.js
+++ src/game/demo/demo.js
@@ -1,9 +1,27 @@
 import * as v from 'engine/index';
 
-export class MainScene extends v.Control {
+// export class MainScene extends v.Control {
+//     static instance() { return new MainScene }
+
+//     _ready() { }
+// }
+
+// v.attach_script('res://scene/demo.tscn', MainScene);
+
+
+
+
+
+export class MainScene extends v.Spatial {
     static instance() { return new MainScene }
 
-    _ready() { }
+    _ready() {
+        this.cube = this.get_node("cube");
+        this.set_process(true);
+    }
+    _process(delta) {
+        this.cube.rotate_y(Math.PI * 0.4 * delta);
+    }
 }
 
-v.attach_script('res://scene/demo.tscn', MainScene);
+v.attach_script('res://scene/demo_3d.tscn', MainScene);
