648360706ee441dc982cb83d9056429ac3b7161f
Sean Bohan <pixelpicosean@gmail.com>
2020-12-27 16:01:10 +0800

    Copy fonts.css to dist folder for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,19 @@
-const { merge } = require("webpack-merge");
+const path = require("path");
+
 const common = require("./webpack.common.js");
+
+const { merge } = require("webpack-merge");
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                "fonts.css",
+            ],
+        }),
+    ],
     optimization: {
         minimize: true,
         minimizer: [
